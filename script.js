e4e62188cce6c4f7b955b88c262230f8fa96b5e3
Max Base <MaxBaseCode@Gmail.Com>
2022-03-26 03:51:39 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function start() {
+  // navigator.vibrate([500]);
+  navigator.vibrate(500);
+}
+
+function startPattern() {
+  // Values at even indices (0, 2, 4, ...) specify vibrations, while the odd indices specify pauses.
+  // Vibrate for 500ms 6 times, pausing for 250ms in between each one.
+  navigator.vibrate([
+    500, 250,
+    500, 250,
+    500, 250,
+    500, 250,
+    500, 250,
+    500
+  ]);
+}
+
+function stopVibrations() {
+  navigator.vibrate(0);
+}
